16cf2a7da8c71015c6750e8bd1163522c4a0208d
Arifulislam332 <ayatullaharif332@gmail.com>
2024-07-28 18:54:35 +0600

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import type { Metadata } from "next";
+import { Rubik } from "next/font/google";
+import "./globals.css";
+
+const rubik = Rubik({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Vehicle Vista",
+  description: "Vehicle and vehicle equipment web",
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html>
+      <body className={rubik.className}>
+        <main className="min-h-screen flex flex-col">
+          <Header />
+          <div className="flex-1">{children}</div>
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
