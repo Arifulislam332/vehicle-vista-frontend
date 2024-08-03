895bbaaacd3330296bbfca71e8d98141667f3d06
Arifulislam332 <ayatullaharif332@gmail.com>
2024-08-04 00:45:36 +0600

    feat:create user req

--- src/providers/MyAuth0Provider.tsx
+++ src/providers/MyAuth0Provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useCreateUser } from "@/hooks/useCreateUser";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 import { PropsWithChildren } from "react";
 
@@ -8,11 +9,17 @@ const MyAuth0Provider = ({ children }: PropsWithChildren) => {
   const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID;
   const callbackUri = process.env.NEXT_PUBLIC_AUTH0_URI;
 
+  const { createUser } = useCreateUser();
+
   if (!domain || !clientId || !callbackUri) {
     throw new Error("Unable to initialize auth0");
   }
 
-  const onRedirectCallback = (AppState?: AppState, User?: User) => {};
+  const onRedirectCallback = (AppState?: AppState, user?: User) => {
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
+  };
 
   return (
     <Auth0Provider
