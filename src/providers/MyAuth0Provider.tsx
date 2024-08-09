d8563b7db2311b6023a7dd33c1d1a51187f2c5f7
Arifulislam332 <ayatullaharif332@gmail.com>
2024-08-09 22:45:41 +0600

    audience added

--- src/providers/MyAuth0Provider.tsx
+++ src/providers/MyAuth0Provider.tsx
@@ -1,24 +1,23 @@
 "use client";
 
-import { useCreateUser } from "@/hooks/useCreateUser";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { AppState, Auth0Provider } from "@auth0/auth0-react";
+import { useRouter } from "next/navigation";
 import { PropsWithChildren } from "react";
 
 const MyAuth0Provider = ({ children }: PropsWithChildren) => {
   const domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN;
   const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID;
   const callbackUri = process.env.NEXT_PUBLIC_AUTH0_URI;
+  const audience = process.env.NEXT_PUBLIC_AUTH0_AUDIENCE;
 
-  const { createUser } = useCreateUser();
+  const router = useRouter();
 
-  if (!domain || !clientId || !callbackUri) {
+  if (!domain || !clientId || !callbackUri || !audience) {
     throw new Error("Unable to initialize auth0");
   }
 
-  const onRedirectCallback = (AppState?: AppState, user?: User) => {
-    if (user?.sub && user?.email) {
-      createUser({ auth0Id: user.sub, email: user.email });
-    }
+  const onRedirectCallback = (appState?: AppState) => {
+    router.push(appState?.returnTo || "/auth-callback");
   };
 
   return (
@@ -27,6 +26,7 @@ const MyAuth0Provider = ({ children }: PropsWithChildren) => {
       clientId={clientId}
       authorizationParams={{
         redirect_uri: callbackUri,
+        audience,
       }}
       onRedirectCallback={onRedirectCallback}
     >
